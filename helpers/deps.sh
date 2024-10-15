3f0f61b199d8f2fd561527c8c8707d0d8cec2bfa
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2024-10-15 18:22:56 +0000

    Rework dependency check to be more intuitive

--- helpers/deps.sh
+++ helpers/deps.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-exec which -a \
-    curl \
-    python \
-    losetup \
-    kpartx \
-    partx \
-    mount \
-    umount \
-    qemu-img \
-    tar \
-    gzip
+EXIT_CODE=0
+
+while read -r util; do
+    util_path=$(which "${util}")
+    EXIT_CODE=$(( $? || ${EXIT_CODE}))
+    echo "${util} => ${util_path}"
+done <<'EOF'
+curl
+python
+losetup
+kpartx
+partx
+mount
+umount
+qemu-img
+tar
+gzip
+EOF
+
+exit ${CODE}
